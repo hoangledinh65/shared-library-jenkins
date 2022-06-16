00870de878f827181131fb21e2bc0c48ce59f452
Hoang DinhLe <hoang.dinhle@nttdata.com>
2022-06-16 15:24:56 +0700

    update

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -1,4 +1,6 @@
-def call(message) {
+def call(String message, String person, boolean toggle) {
     echo "this is message from my library: ${message}"
     sh 'mvn --version'
+    echo "Hello ${person}"
+    echo "Toggle: ${toggle}"
 }
