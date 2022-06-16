279b5a6c36948c957b4fb8fb28b1e2a6f6850235
Hoang DinhLe <hoang.dinhle@nttdata.com>
2022-06-16 14:07:36 +0700

    update

--- src/org/hoangtest/MPoint.groovy
+++ src/org/hoangtest/MPoint.groovy
@@ -3,15 +3,9 @@ package org.foo
 // point in 3D space
 class MPoint {
   float x,y,z
-  public MPoint (float xvalue) {
-    x = xvalue;
-    y = 2.4f;
-    z = 3.0f;
-  }
 
   def abc() {
     sh 'echo abcd'
   }
-  return this
 
 }
