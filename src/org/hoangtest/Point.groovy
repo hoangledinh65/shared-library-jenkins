21617a13f864e9eea193c0e9d0cf0e385d97e4c9
Hoang DinhLe <hoang.dinhle@nttdata.com>
2022-06-16 13:32:05 +0700

    update

--- src/org/hoangtest/Point.groovy
+++ src/org/hoangtest/Point.groovy
@@ -3,4 +3,12 @@ package org.foo
 // point in 3D space
 class Point {
   float x,y,z
+  public Point (float xvalue) {
+    x = xvalue;
+    y = 2.4f;
+    z = 3.0f;
+  }
+  public String echo() {
+    return x + " " + y + " " + z;
+  }
 }
